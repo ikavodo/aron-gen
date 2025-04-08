2ee0b40562f6797e0872e419264c0bf76a10a514
ikavodo <ido.akov1@alumni.aalto.fi>
2025-04-08 14:43:16 +0200

    post-blogpost 2

--- aron_helper.py
+++ aron_helper.py
@@ -1,13 +1,27 @@
-from itertools import islice
+from copy import deepcopy
+import random
+from timeit import default_timer
+from itertools import islice, chain, combinations
+
 from num2words import num2words
-import string
-import sys
 
 # consts
 BASE_START = " is the "
 BASE_END = " letter"
 # use this for finding sequences starting with ordinals outside of prefix/suffix range
 SEARCH_LOWER_BOUND = max(len(BASE_START.replace(" ", "")), len(BASE_END.replace(" ", "")))
+
+
+# Define the custom exception class
+class VerifierError(Exception):
+    def __init__(self, message="Verifier failed", input_data=None):
+        self.message = message
+        self.input_data = input_data  # Store the input that caused the failure
+        super().__init__(self.message)
+
+    def __str__(self):
+        # Customize the string representation to include input data
+        return f"{self.message}: {self.input_data}"
 
 
 # nifty func to be used for simpler analysis- 1-> 1st, 2-> 2nd etc...
@@ -26,15 +40,13 @@ def n2w(n):
     return os.replace(", ", "").replace(" ", "").replace("-", "")
 
 
-def pows_two():
-    last = 1
-    l = [last]
-    while True:
-        last *= 2
-        if last >= sys.maxsize:
-            break
-        l.append(last)
-    return l
+# Helper- generates sequence from generator and input
+def aronson(n, letter, forward=True, inp=None):
+    # generates 'the rest' from input
+    seq = list(islice(agen_better(letter, forward=forward), n)) if inp is None else \
+        inp + list(islice(agen_better(letter, forward=forward, inp=inp), n - len(inp)))
+    # bug: if input is of length n then list(islice(...,0)) doesn't evaluate generator- no VerifierError.
+    return seq
 
 
 # Extension of Michael Branicky's implementation in OEIS, allowing for generating backwards-true sequences, as well
@@ -42,9 +54,9 @@ def pows_two():
 # see https://oeis.org/A005224
 def agen_better(letter, forward=True, inp=None):
     def update_from_inp(forward, inp, letter, s):
-        # check is correct
-        if not verifier(letter, inp, forward):
-            raise ValueError('incorrect inputs')
+        # check is correct, be pessimistic
+        if not verifier(letter, inp, forward, True):
+            raise VerifierError()
         for i in inp:
             s += (n2w(i) if forward else n2w(i)[::-1])
         idx = max(inp)  # inp[-1] if monotonically rising
@@ -62,47 +74,79 @@ def agen_better(letter, forward=True, inp=None):
             # Find the next occurrence of the letter. If not inside s.find() returns -1, so repeats infinitely
             idx_rel = 1 + s.find(letter)
         except ValueError:
+            # not sure this is necessary
             break  # Stop if letter is not found
         idx += idx_rel
         yield idx
         # Adjust the sequence for the next iteration
         s = s[idx_rel:] + (n2w(idx) if forward else n2w(idx)[::-1])
 
-#correct but slow because requires generating the entire sequence
+
+# correct but slow because requires generating the entire sequence
 def verifier_slow(letter, indices, forward=True):
     sequence = get_sequence(letter, indices, forward)
     # Reverse the sequence if forward is False
     if not forward:
         sequence = sequence[::-1]
     # Check if the input letter matches at the specified indices
-    return all(sequence[i - 1] == letter for i in indices)
+    try:
+        return all(sequence[i - 1] == letter.lower() for i in indices)
+    except IndexError:
+        return False
 
 
-#only works for backwards refering sequences
-def verifier(letter, indices, forward=True):
+def test_verifiers(letter, n, forward, *verifiers):
+    seq = list(islice(agen_better(letter, forward=forward), n))  # Assuming agen_better is defined elsewhere
+    execution_times = {}
+
+    # Loop over verifiers
+    for _ in range(n):
+        # Generate a new random seq_copy for each iteration
+        seq_copy = deepcopy(seq)
+        random_int = random.randint(1, n - 1)
+        seq_copy[(random_int - 1):(random_int + 1)] = range((seq_copy[random_int] - 1), (seq_copy[random_int] + 1))
+
+        # Loop over provided verifiers
+        for verifier in verifiers:
+            # Time the current verifier using the same seq_copy
+            start_time = default_timer()
+            # make sure no such sequence is actually correct
+            verifier(letter, seq_copy, forward)
+            end_time = default_timer()
+
+            # Store the execution time for the verifier
+            execution_times.setdefault(verifier.__name__, []).append(end_time - start_time)
+
+    # Calculate and print average times
+    for verifier_name, times in execution_times.items():
+        avg_time = sum(times) / n
+        print(f"Average execution time for {verifier_name}: {avg_time:.4f} seconds")
+
+
+def verifier(letter, indices, forward=True, forwards_referring=False):
+    if forwards_referring:
+        return verifier_slow(letter, indices, forward)
+    else:
+        # prior knowledge about monotonicity allows function to run faster
+        return verifier_fast(letter, indices, forward)
+
+
+# only works for non-forwards referring sequences!
+def verifier_fast(letter, indices, forward=True):
     # Determine the initial sequence based on the direction (forward or reverse)
     s = (letter + BASE_START).replace(" ", "") if forward else BASE_END.replace(" ", "")[::-1]
-    prev_idx = 0
-    is_mono = all(indices[i] < indices[i + 1] for i in range(len(indices) - 1))
     for idx in indices:
-        # For monotonic sequences, compute relative index
-        idx_rel = idx - prev_idx if is_mono else 0
-
         # Add the ordinal to the sequence
         s += (n2w(idx) if forward else n2w(idx)[::-1])
-
         try:
             # Check if the letter matches the expected position
-            if s[(idx_rel if is_mono else idx) - 1] != letter:
+            if s[idx - 1] != letter.lower():
                 return False
         except IndexError:
             # If the index is out of range, return False
             return False
-
-        prev_idx = idx
-        s = s[idx_rel:]  # Adjust sequence for next iteration (monotonic case)
-
     return True
+
 
 # Generate sentence from sequence input
 def get_sequence(letter, indices, forward=True, delimited=False):
@@ -119,7 +163,40 @@ def print_sequence(letter, indices, forward=True):
     print(get_sequence(letter, indices, forward, True))
 
 
-# Need to understand what I'm doing. a is the letter-> a is an outlier
+def generate_variations(n, letter, forward=True, inp=None):
+    # where to start generating variations from
+    orig = aronson(n, letter, forward, inp)
+    start_idx = -1
+    stack = [(orig, start_idx)]
+    while stack:
+        # keep track of which idx was omitted
+        cur, cur_idx = stack.pop()
+        yield cur
+        # only remove indices forward
+        for idx in range(cur_idx + 1, n - 1):
+            # important: keep track of which index was used to generate the current sequence
+            inp = cur[:idx] + [cur[idx + 1]]
+            try:
+                extend = aronson(n, letter, forward, inp)
+            except VerifierError:
+                # verifier returned false- meaning sequence is incorrect and can't be extended
+                continue
+            stack.append((extend, idx))
+
+
+def intersect_aronson_sets(n, letter):
+    # helper
+    def power_set(nums):
+        return set(chain.from_iterable(combinations(nums, r) for r in range(len(nums) + 1)))
+
+    forwards = aronson(n, letter, True)
+    backwards = aronson(n, letter, False)
+    # first look at sub-sequence intersection
+    intersection = power_set(forwards) & power_set(backwards)
+    return {i for i in intersection if verifier(letter, i, True) and verifier(letter, i, False)}
+
+
+# These two will be addressed next time. a is the letter-> a is an outlier
 def is_outlier(letter):
     s_pref, s_suff = (letter + BASE_START).replace(" ", ""), BASE_END.replace(" ", "")
     s = s_pref + s_suff
@@ -156,48 +233,3 @@ def gen_outliers(letter):
         # Update the seen indices and new indices for the next iteration
         seen_idxs.update(next_indices)
         new_indices = next_indices  # Set new_indices for the next loop
-
-
-#
-# #Iteratively adds and checks ordinals, for non-monotonic examples, such as 't is the twelfth, seventh letter' (->)
-# def verifier(letter, indices, forward=True):
-#     s = (letter + BASE_START).replace(" ", "") if forward else BASE_END.replace(" ", "")[::-1]
-#     for idx in indices:
-#         s += (n2w(idx) if forward else n2w(idx)[::-1])
-#         try:
-#             # Find the next occurrence of the letter
-#             if s[idx-1] != letter:
-#                 return False
-#         except IndexError:
-#             # Inherently false
-#             return False
-#         # Adjust the sequence for the next iteration
-#     return True
-#
-#
-# # This should be used for monotonic sequences- more space efficient. Otherwise this should be put inside the other one,
-# # where the other one builds the string iteratively in entirety
-# def verifier_monotonic(letter, indices, forward=True):
-#     #problem- what if receive a generator?
-#     # if len(indices)>1:
-#     #     #check if monotonically increasing
-#     #     if not all(indices[i] <= indices[i+1] for i in range(len(indices) - 1)):
-#     #         print('Use general verifier method')
-#     #         return None
-#
-#     s = (letter + BASE_START).replace(" ", "") if forward else BASE_END.replace(" ", "")[::-1]
-#     prev_idx = 0
-#     for idx in indices:
-#         idx_rel = idx - prev_idx
-#         s += (n2w(idx) if forward else n2w(idx)[::-1])
-#         try:
-#             # Find the next occurrence of the letter
-#             if s[idx_rel - 1] != letter:
-#                 return False
-#         except IndexError:
-#             #Inherently false
-#             return False
-#         s = s[idx_rel:]
-#         prev_idx = idx
-#         # Adjust the sequence for the next iteration
-#     return True

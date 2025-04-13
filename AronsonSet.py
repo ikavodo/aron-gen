46c58a523fd5dc449cf1d3bc9c75de8e61a4eaae
ikavodo <ido.akov1@alumni.aalto.fi>
2025-04-14 01:08:19 +0200

    Added brute force for minimal testing, set operations

--- AronsonSet.py
+++ AronsonSet.py
@@ -1,6 +1,7 @@
-from itertools import islice, cycle, combinations, permutations
+from itertools import islice, combinations, permutations
 from AronsonSequence import AronsonSequence, n2w, Refer, Direction, LEN_PREFIX, LEN_SUFFIX
 from collections import defaultdict
+from typing import Callable
 
 # upper bound for searching for singleton Aronson sequences
 SINGLETON_UPPER = 40
@@ -281,10 +282,10 @@ class AronsonSet:
 
     # For generating ground truths up to n=3 and comparing missing sets in other implementation
     def generate_brute_force(self, n_iterations: int):
-        def generate_unique_lists(n, upper_bound):
-            if n > upper_bound:
+        def generate_unique_lists(n, bound):
+            if n > bound:
                 raise ValueError("n can't be greater than the number of available unique values")
-            return [list(p) for p in permutations(range(1, upper_bound + 1), n)]  # Start from 1
+            return [list(p) for p in permutations(range(1, bound + 1), n)]  # Start from 1
 
         if n_iterations < 0:
             raise ValueError("Num of iterations must be non-negative")
@@ -293,7 +294,6 @@ class AronsonSet:
             # do nothing
             return
 
-        compute_upper = lambda x: self.cur_iter * ORD_TABLE[x] + 2 * max(LEN_PREFIX, LEN_SUFFIX)
         cur_ord_key = 1  # just in case. For 0 -> upper is
         while self.cur_iter < n_iterations:
             # enlarge search range
@@ -302,54 +302,74 @@ class AronsonSet:
             upper_bound = self.cur_iter * ORD_TABLE[cur_ord_key] + 2 * max(LEN_PREFIX, LEN_SUFFIX)
             if upper_bound >= 10 ** (cur_ord_key + 1):
                 cur_ord_key += 1
-            seqs = (AronsonSequence(self.letter, list(l), self.direction) for l in
+            seqs = (AronsonSequence(self.letter, list(perm), self.direction) for perm in
                     generate_unique_lists(self.cur_iter, upper_bound))
             cur_seqs = {s for s in seqs if self.is_correct(s)}
             self.sets_by_iteration[self.cur_iter] = cur_seqs
             # total sets generated so far
             self.seen_seqs.update(cur_seqs)
 
-            #
-            # # do these later
-            # def intersect_aronson_sets(self, other: 'AronsonSet', n: int, seq1: AronsonSequence = None,
-            #                            seq2: AronsonSequence = None):
-            #     """
-            #     Computes the intersection of two AronsonSets by comparing their generated sequences
-            #     and finding common subsets.
-            #
-            #     :param other: The other AronsonSet to intersect with.
-            #     :param n: The length of the sequences to generate.
-            #     :param seq1: An optional input AronsonSequence for the first generator.
-            #     :param seq2: An optional input AronsonSequence for the second generator.
-            #     :return: A set of common valid AronsonSequences that belong to both sets.
-            #     """
-            #     # can use self.seen_seqs and other.seen_seqs here! More elegant
-            #     self_set, other_set = set(), set()
-            #     # generate all sequences up to length n from each generator
-            #     for i in range(n):
-            #         for seq1, seq2 in zip(self.generate_variations(i, seq1), other.get_generator(i, seq2)):
-            #             self_set.update(seq1)
-            #             other_set.update(seq2)
-            #     # intersection of sequences generated
-            #     common_subsets = self_set & other_set
-            #
-            #     return {subset for subset in common_subsets if self.is_correct(subset) and other.is_correct(subset)}
-            #
-            # def union(self, other_set: 'AronsonSet', n: int):
-            #     """
-            #     Yields all unique AronsonSequences of length n from both sets in alternating order.
-            #
-            #     :param other_set: The other AronsonSet to union with.
-            #     :param n: The length of the sequences to generate.
-            #     :return: A generator yielding unique AronsonSequences from both sets.
-            #     """
-            #     seen = set()
-            #     generators = [self.get_generator(n), other_set.get_generator(n)]
-            #     for g in cycle(generators):
-            #         try:
-            #             seq = next(g)
-            #             if seq not in seen:
-            #                 seen.add(seq)
-            #                 yield seq
-            #         except StopIteration:
-            #             break
+    @staticmethod
+    def _set_operation_core(set1: 'AronsonSet', set2: 'AronsonSet', n: int, ignore_letters: bool,
+                            set_op: Callable[[set, set], set]):
+        """
+        Core logic for set operations on AronsonSets.
+        """
+        set1.generate_from_rules(n)
+        set2.generate_from_rules(n)
+
+        get_repr = (lambda s: s.get_elements()) if ignore_letters else (lambda s: s)
+
+        elems1 = {get_repr(s) for s in set1.get_seen_seqs()}
+        elems2 = {get_repr(s) for s in set2.get_seen_seqs()}
+
+        combined = set_op(elems1, elems2)
+
+        if ignore_letters:
+            letter = 'χ'  # or 'Π'
+            return {
+                (AronsonSequence(letter, el, Direction.FORWARD),
+                 AronsonSequence(letter, el, Direction.BACKWARD))
+                for el in combined
+            }
+
+        if set1.display_letter != set2.display_letter:
+            raise ValueError("Mismatched letters. Use ignore_letters=True to override.")
+
+        return {
+            (AronsonSequence(set1.display_letter, el, Direction.FORWARD),
+             AronsonSequence(set1.display_letter, el, Direction.BACKWARD))
+            for el in combined
+        }
+
+    # Set operations utilize _set_operation_core() helper method
+    # Intersection
+    def intersect_aronson_sets(self, other: 'AronsonSet', n: int, ignore_letters: bool = False):
+        return self._set_operation_core(self, other, n, ignore_letters, set.intersection)
+
+    @staticmethod
+    def intersect_aronson_sequences(seq1: AronsonSequence, seq2: AronsonSequence, n: int, ignore_letters: bool = False):
+        set1 = AronsonSet.from_sequence(seq1)
+        set2 = AronsonSet.from_sequence(seq2)
+        return AronsonSet._set_operation_core(set1, set2, n, ignore_letters, set.intersection)
+
+    # Union
+    def union_aronson_sets(self, other: 'AronsonSet', n: int, ignore_letters: bool = False):
+        return self._set_operation_core(self, other, n, ignore_letters, set.union)
+
+    @staticmethod
+    def union_aronson_sequences(seq1: AronsonSequence, seq2: AronsonSequence, n: int, ignore_letters: bool = False):
+        set1 = AronsonSet.from_sequence(seq1)
+        set2 = AronsonSet.from_sequence(seq2)
+        return AronsonSet._set_operation_core(set1, set2, n, ignore_letters, set.union)
+
+    # Difference
+    def difference_aronson_sets(self, other: 'AronsonSet', n: int, ignore_letters: bool = False):
+        return self._set_operation_core(self, other, n, ignore_letters, set.difference)
+
+    @staticmethod
+    def difference_aronson_sequences(seq1: AronsonSequence, seq2: AronsonSequence, n: int,
+                                     ignore_letters: bool = False):
+        set1 = AronsonSet.from_sequence(seq1)
+        set2 = AronsonSet.from_sequence(seq2)
+        return AronsonSet._set_operation_core(set1, set2, n, ignore_letters, set.difference)

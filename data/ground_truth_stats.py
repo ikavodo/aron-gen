3b4affa1d08d0c46962a1e046b86986a16f431e3
ikavodo <ido.akov1@alumni.aalto.fi>
2025-05-16 14:37:29 +0200

    main.py script for running on cluster, added is_empty() method to AronsonSet class

---
+++ data/ground_truth_stats.py
@@ -0,0 +1,15 @@
+# Ground truth for number of sets per iteration:
+# Direction.FORWARD
+# n = 0: 1
+# n = 1: 8
+# n = 2: 73
+# n = 3: 955
+
+# Direction.BACKWARD
+# n = 0: 1
+# n = 1: 7
+# n = 2: 67
+# n = 3: 771
+
+forward_stats = {0: 1, 1: 8, 2: 73, 3: 955}
+backward_stats = {0: 1, 1: 7, 2: 67, 3: 771}
